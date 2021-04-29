010f88c3314a1b5e05da233113c90556a0c20690
Bisrat <admin@example.com>
2021-04-29 11:40:07 +0100

    repository for characters implemented

---
+++ app/src/main/java/com/orderpay/codetest/repository/CharactersRepository.kt
@@ -0,0 +1,20 @@
+package com.orderpay.codetest.repository
+
+import com.orderpay.codetest.database.AppDataBase
+import com.orderpay.codetest.network.ApiServices
+import com.orderpay.codetest.utils.OpenForTesting
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+@OpenForTesting
+class CharactersRepository(private val apiServices: ApiServices, private val database : AppDataBase) {
+
+    suspend fun refreashCharacters() {
+        withContext(Dispatchers.IO) {
+            Timber.d("refresh characters is called")
+            val characterList = apiServices.getAllCharacters().await()
+            database.characterDao.insertAll(characterList)
+        }
+    }
+}
